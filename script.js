ad9d1ed9124c9b2210ffaf78a7aa0295254b13cb
gmf-uah <gmf0010@uah.edu>
2025-05-18 03:41:42 -0500

    added fuse-js and messed with css

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // import "./index.html";
 // import "./style.css";
 
+const fuse = new Fuse(cssColors)
+console.log(fuse.search("antiq"))
+
 let colorInputBox = document.getElementById("color-input");
-colorInputBox.placeholder = "test"
+colorInputBox.placeholder = cssColors[Math.floor(Math.random() * cssColors.length)];
 colorInputBox.borderRadius = (colorInputBox.offsetHeight / 2)
 
 colorInputBox.addEventListener("input", function(event) {
+
     console.log(event.target.value);
 })
